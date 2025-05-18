180e1a43d7bf8964e1c8aac9c438889f3ac10120
helmutsirait77 <helmutsirait77@gmail.com>
2025-05-18 12:54:22 +0700

    web-portfolio: update some contact and  fixed layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-// hamburger menu 
+ 
 let hamMenu = document.getElementById('menu');
 let nav = document.querySelector('nav');
 const header = document.querySelector('header');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header a');
+let revealRight = document.querySelectorAll('.reveal-right');
+let revealLeft = document.querySelectorAll('.reveal-left');
 
+
+// hamburger menu
 hamMenu.addEventListener('click', function() {
       hamMenu.classList.toggle('active');
       nav.classList.toggle('active');
@@ -10,10 +16,7 @@ hamMenu.addEventListener('click', function() {
 }); 
 
 // scroll sections active links 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header a');
-
-window.onscroll = function() {
+window.addEventListener('scroll', function() {
 	 sections.forEach(function(sec) {
            let top = window.scrollY;
            let offset = sec.offsetTop - 150;
@@ -26,6 +29,9 @@ window.onscroll = function() {
                        document.querySelector('header  a[href*='+ id +']').classList.add('active');
                });
            }; 
+
+
+
 	 });
      // sticky navbar 
      let header = document.querySelector('header');
@@ -35,51 +41,82 @@ window.onscroll = function() {
       hamMenu.classList.remove('active');
       nav.classList.remove('active');
       header.classList.remove('active');
+       
+
+     const windowHeight = window.innerHeight; 
+     const elementVisible = 150;
+
+    for( let rl = 0; rl < revealLeft.length; rl++ ) {
+            const revealTopLeft = revealLeft[rl].getBoundingClientRect().top;
+             (revealTopLeft < windowHeight - elementVisible) ? revealLeft[rl].classList.add('active') : revealLeft[rl].classList.remove('active');
+       } 
+
+    for( let rr = 0; rr < revealRight.length; rr++ ) {
+            const revealTopRight = revealRight[rr].getBoundingClientRect().top;
+             (revealTopRight < windowHeight - elementVisible) ? revealRight[rr].classList.add('active') : revealRight[rr].classList.remove('active');
+       } 
 
 
-      
-}   
+}); 
+  
+
+// function reveal() { 
+        
+//          for(let i = 0; i < revealLeft.length; i++) {
+                
+//                  let revealTop = revealLeft[i].getBoundingClientRect().top;
+
+//                  if( revealTop < windowHeight - elementVisible ) {
+//                      revealLeft[i].classList.add('active');
+//                  }else {
+//                      revealLeft[i].classList.remove('active');
+//                  }
+//              }
+
+// } 
+
+
   
 // typed js 
-const typed = document.querySelector('.multiple-text');
-const toType = ['Frontend Developer', 'Web Design', 'Blogger'];
+// const typed = document.querySelector('.multiple-text');
+// const toType = ['Frontend', 'Web Design', 'Blogger'];
 
-const delayTypingChar = 100;
-const delayErasingText = 100;
-const delayTypingText = 1000;
+// const delayTypingChar = 100;
+// const delayErasingText = 100;
+// const delayTypingText = 1000;
 
-let totypeIndex = 0;
-let charIndex = 0;
+// let totypeIndex = 0;
+// let charIndex = 0;
 
-function typeText() {
-   if (charIndex < toType[totypeIndex].length) {
-      typed.textContent += toType[totypeIndex].charAt(charIndex);
-      charIndex++;
-      setTimeout(typeText, delayTypingChar);
-   }
-   else {
-      setTimeout(eraseText, delayTypingText);
-   }
-}
+// function typeText() {
+//    if (charIndex < toType[totypeIndex].length) {
+//       typed.textContent += toType[totypeIndex].charAt(charIndex);
+//       charIndex++;
+//       setTimeout(typeText, delayTypingChar);
+//    }
+//    else {
+//       setTimeout(eraseText, delayTypingText);
+//    }
+// }
 
-function eraseText() {
-   if (charIndex > 0) {
-      typed.textContent = toType[totypeIndex].substring(0, charIndex-1);
-      charIndex = charIndex-1;
-      setTimeout(eraseText, delayErasingText);
-   }
-   else {
-      totypeIndex++;
+// function eraseText() {
+//    if (charIndex > 0) {
+//       typed.textContent = toType[totypeIndex].substring(0, charIndex-1);
+//       charIndex = charIndex-1;
+//       setTimeout(eraseText, delayErasingText);
+//    }
+//    else {
+//       totypeIndex++;
 
-      if (totypeIndex >= toType.length)
-         totypeIndex = 0;
-         setTimeout(typeText, delayTypingText);
-   }
-}
+//       if (totypeIndex >= toType.length)
+//          totypeIndex = 0;
+//          setTimeout(typeText, delayTypingText);
+//    }
+// }
 
-window.onload = function() {
-   if (toType[totypeIndex].length) setTimeout(typeText, delayTypingText);
-}   
+// window.onload = function() {
+//    if (toType[totypeIndex].length) setTimeout(typeText, delayTypingText);
+// }   
 
 // smooth scroll 
 navLinks.forEach(function (i) {
